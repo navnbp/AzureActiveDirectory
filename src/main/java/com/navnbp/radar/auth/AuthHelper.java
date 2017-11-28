840f3a59e1e452519de516199d694bb7eb9df671
Naveen <navn@Naveens-MacBook-Pro.local>
2017-11-27 20:38:10 -0600

    initial commit

---
+++ src/main/java/com/navnbp/radar/auth/AuthHelper.java
@@ -0,0 +1,34 @@
+package com.navnbp.radar.auth;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.nimbusds.openid.connect.sdk.AuthenticationResponse;
+import com.nimbusds.openid.connect.sdk.AuthenticationSuccessResponse;
+
+public final class AuthHelper {
+
+	public static final String PRINCIPAL_SESSION_NAME = "principal";
+
+	private AuthHelper() {
+	}
+
+	public static boolean isAuthenticated(HttpServletRequest request) {
+		return request.getSession().getAttribute(PRINCIPAL_SESSION_NAME) != null;
+	}
+
+	public static AuthenticationResult getAuthSessionObject(HttpServletRequest request) {
+		return (AuthenticationResult) request.getSession().getAttribute(PRINCIPAL_SESSION_NAME);
+	}
+
+	public static boolean containsAuthenticationData(HttpServletRequest httpRequest) {
+		return httpRequest.getMethod().equalsIgnoreCase("POST")
+				&& (httpRequest.getParameterMap().containsKey(AuthParameterNames.ERROR)
+						|| httpRequest.getParameterMap().containsKey(AuthParameterNames.ID_TOKEN)
+						|| httpRequest.getParameterMap().containsKey(AuthParameterNames.CODE));
+	}
+
+	public static boolean isAuthenticationSuccessful(AuthenticationResponse authResponse) {
+		return authResponse instanceof AuthenticationSuccessResponse;
+	}
+}
